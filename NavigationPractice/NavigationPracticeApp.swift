cca8f23347b26ba784014bad540d4441f6cf2464
Marnie Reid <marnie@kodewithklossy.com>
2023-07-07 09:29:41 -0700

    Initial Commit

---
+++ NavigationPractice/NavigationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPracticeApp.swift
+//  NavigationPractice
+//
+//  Created by scholar on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
